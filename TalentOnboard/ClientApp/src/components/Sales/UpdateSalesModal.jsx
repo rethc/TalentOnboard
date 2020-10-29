fd617cd64c64f28eac1a2219b2841c6351bdd005
rethc <reth@xtra.co.nz>
2020-10-30 00:29:12 +1300

    Working on update sales view

--- TalentOnboard/ClientApp/src/components/Sales/UpdateSalesModal.jsx
+++ TalentOnboard/ClientApp/src/components/Sales/UpdateSalesModal.jsx
@@ -1,45 +1,47 @@
 import React, { Component } from 'react';
-import { Button, Modal, Form } from 'semantic-ui-react'
+import { Button, Header, Modal, Form } from 'semantic-ui-react'
 import axios from 'axios';
 
-export default class CreateCustomerModal extends Component {
+export default class UpdateSalesModal extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { customerName: '', customerAddress: '', modalOpen: false };
+    this.state = {
+      productID: this.props.details.id,
+      productName: this.props.details.name,
+      productPrice: this.props.details.price,
+      modalOpen: false
+    };
 
     this.handleName = this.handleName.bind(this);
-    this.handleAddress = this.handleAddress.bind(this);
+    this.handlePrice = this.handlePrice.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event) {
-    this.addNewCustomer();
-    this.handleClose();
+  componentDidMount() {
   }
 
   // Name Text Handler
   handleName(event) {
-    this.setState({ customerName: event.target.value });
+    this.setState({ productName: event.target.value });
   }
 
-  // Address Text Handler
-  handleAddress(event) {
-    this.setState({ customerAddress: event.target.value });
+  // Price Text Handler
+  handlePrice(event) {
+    this.setState({ productPrice: event.target.value });
   }
-
-
 
   // Modal Button Handler
   handleClose = () => this.setState({ modalOpen: false })
   handleOpen = () => this.setState({ modalOpen: true })
 
+  handleSubmit(event) {
+    this.updateProductDetails();
+    this.handleClose();
+  }
 
-  addNewCustomer = () => {
-    axios.post("Customers/PostCustomers", {
-      name: this.state.customerName,
-      address: this.state.customerAddress
-    })
+  updateProductDetails = () => {
+    axios.put(`Products/PutProducts/${this.state.productID}`, { id: this.state.productID, name: this.state.productName, price: this.state.productPrice })
       .then((result) => {
         this.props.updateTable();
       })
@@ -49,28 +51,29 @@ export default class CreateCustomerModal extends Component {
   }
 
   render() {
+
     return (
       <Modal
         onClose={this.handleClose}
         onOpen={this.handleOpen}
         open={this.state.modalOpen}
         size='tiny'
-        trigger={<Button primary>New Customer</Button>}
+        trigger={<Button>Edit</Button>}
       >
-        <Modal.Header>Create new customer</Modal.Header>
+        <Modal.Header>Edit Product</Modal.Header>
         <Modal.Content >
           <Modal.Description>
+            <Header>{this.state.productName} </Header>
             <Form>
               <Form.Field>
-                <label>Full Name</label>
-                <input type="text" placeholder="Name" value={this.state.customerName}
+                <label>Product Name</label>
+                <input type="text" value={this.state.productName}
                   onChange={this.handleName} />
-
               </Form.Field>
               <Form.Field>
-                <label>Address</label>
-                <input type="text" placeholder="Address" value={this.state.customerAddress}
-                  onChange={this.handleAddress} />
+                <label>Price</label>
+                <input type="number" value={this.state.productPrice}
+                  onChange={this.handlePrice} />
               </Form.Field>
             </Form>
           </Modal.Description>
@@ -78,17 +81,17 @@ export default class CreateCustomerModal extends Component {
         <Modal.Actions>
           <Button color='black' onClick={this.handleClose}>
             Close
-            
         </Button>
           <Button
             type='submit'
-            content="Create"
-            icon='user plus'
+            content="Confirm"
+            icon='checkmark'
             labelPosition='right'
             onClick={this.handleSubmit}
-            positive        
+            positive
           />
         </Modal.Actions>
+
       </Modal>
     )
   }
