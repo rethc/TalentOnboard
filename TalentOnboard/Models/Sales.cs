2fa5257d06919b42ec87c1d796b2100caecb13b3
rethc <reth@xtra.co.nz>
2020-11-08 12:36:16 +1300

    Fixed data annotations

--- TalentOnboard/Models/Sales.cs
+++ TalentOnboard/Models/Sales.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -15,11 +14,12 @@ namespace TalentOnboard.Models
         public int ProductId { get; set; }
 
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateSold { get; set; }
-
         public virtual Customers Customer { get; set; }
+
+
         public virtual Products Product { get; set; }
+
         public virtual Stores Store { get; set; }
     }
 }
